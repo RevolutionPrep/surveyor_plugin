9b7463afdefc83c19d104d9065a196051de3ea51
Ryan Moran <ryan.moran@gmail.com>
2010-01-05 12:12:49 -0800

    added more specs for SurveyorModel module and MIT-License

--- spec/surveyor_model_spec.rb
+++ spec/surveyor_model_spec.rb
@@ -4,8 +4,7 @@ describe "SurveyorModel" do
 
 	class TestSurvey < ActiveRecord::Base
 
-		has_survey :api_key => "api_key", :result => :alias_result_id
-		attr_accessor :alias_result_id
+		has_survey :api_key => "api_key", :result => :test_survey_result_id
 
 		def self.columns() @columns ||= []; end
 
@@ -13,10 +12,7 @@ describe "SurveyorModel" do
 			columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
 		end
 
-		column :recommendable_type, :string
-		column :recommendable_id, :integer
-		column :email, :string
-		column :body, :text
+		column :test_survey_result_id, :integer
 
 	end
 
@@ -24,15 +20,57 @@ describe "SurveyorModel" do
 		@test_survey = TestSurvey.new
 	end
 
-	it "should have an API key" do
-		@test_survey.survey_key.should eql("api_key")
-	end
+	describe "ClassMethod" do
 
-	it "should allow for alias attributes of 'surveyor_result_id'" do
-		@test_survey.alias_result_id = 12345
-		@test_survey.surveyor_result_id.should eql(12345)
-	end
+		describe "has_survey(options = {}) class method" do
 
-	it "should retrieve survey results"
+			it "should have an API key" do
+				@test_survey.survey_key.should eql("api_key")
+			end
+
+			it "should allow for alias attributes of 'surveyor_result_id'" do
+				@test_survey.test_survey_result_id = 12345
+				@test_survey.surveyor_result_id.should eql(12345)
+			end
+
+		end
+
+		describe "retrieve_results(instances, options = {}) class method" do
+
+			it "should retrieve survey results"
+
+		end
+
+		describe "retrieve_headcount(instances, handle, choice) class method" do
+
+			it "should retrieve the headcount of those who answered the specified question with the specified response"
+
+		end
+
+		describe "retrieve_percentage_of_respondents(instances, handle, choice) class method" do
+
+			it "should retrieve the percentage of respondents who answered the specified question with the specified response"
+
+		end
+
+		describe "retrieve_percentage_of_total(instances, handle, choice) class method" do
+
+			it "should retrieve the percentage of respondents who answered the specified question with the specified response including non-responses in the total"
+
+		end
+
+		describe "retrieve_gpa_of_respondents(instances, handle) class method" do
+
+			it "should retrieve the GPA of the specified question given a total of people who responded"
+
+		end
+
+		describe "retrieve_gpa_of_total(instances, handle) class method" do
+
+			it "should retrieve the GPA of the specified question given a total of all surveys"
+
+		end
+
+	end
 
 end
